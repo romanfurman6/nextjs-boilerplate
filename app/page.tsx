7eceb962488658292e1e5d01e54a8ce4ec7f44f2
Roman Furman <roman.furman@uptech.team>
2024-11-06 12:46:30 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
+import { NativelyInfo } from "natively";
 import { useNatively } from "natively/useNatively";
 
 export default function Home() {
   // Initialize services
   const natively = useNatively();
-  
+  const nativelyInfo = new NativelyInfo();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button onClick={() => {
@@ -14,6 +15,9 @@ export default function Home() {
       }}>
         Open Natively
       </button>
+      <title>
+        IS ANDROID APP: {nativelyInfo.browserInfo().isAndroidApp}
+      </title>
     </main>
   )
 }

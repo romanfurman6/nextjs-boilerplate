06a3e78dc195bc2caa8fdda9633131c420889faa
Roman Furman <roman.furman@uptech.team>
2024-11-06 15:20:16 +0200

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
-
-import { NativelyInfo } from "natively";
-import { useNatively } from "natively/useNatively";
+import useNatively from 'natively/module/useNatively';
+import { useEffect } from 'react';
 
 export default function Home() {
-  // Initialize services
   const natively = useNatively();
-  const nativelyInfo = new NativelyInfo();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => {
-        natively?.shareText("Hello from Natively")
-      }}>
-        Open Natively
-      </button>
-      <title>
-        IS ANDROID APP: {nativelyInfo.browserInfo().isAndroidApp}
-      </title>
-    </main>
-  )
-}
+
+  useEffect(() => {
+    if (natively) {
+      const nativelyInfo = new natively.features.NativelyInfo();
+      console.log(nativelyInfo.browserInfo());
+
+      // Access Natively type properties or methods
+      // console.log(natively.Natively.someMethod());
+    }
+  }, [natively]);
+
+  return <div>Check the console for browser info.</div>;
+};

6240d4905a65b28083a85c281e6a3f2be6ce05bf
Roman Furman <roman.furman@uptech.team>
2024-11-12 17:34:12 +0200

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
-import { useEffect } from 'react';
 import useNatively from 'natively/useNatively';
 import { NativelyInfo } from 'natively';
-
 
 export default function Home() {
 
   const natively = useNatively();
-  const nativelyInfo = new NativelyInfo();
-  console.log(nativelyInfo.browserInfo());
-  // window.natively.isAndroidApp();
-  useEffect(() => {
-    if (natively) {
-      // const nativelyInfo = new NativelyInfo();
-      
-      natively?.openConsole()
-    }
-  }, [natively]);
-
-  return <div>Check the console for browser info.</div>;
+  const info = new NativelyInfo();
+  const browserInfo = info.browserInfo();
+  const openConsole = () => {
+    natively.openConsole();
+  };
+  console.log(browserInfo);
+  return (
+    <div>
+      Check the console for browser info. isNativeApp: {browserInfo.isNativeApp ? "true" : "false"}
+      <button onClick={openConsole}>Open Console</button>
+    </div>
+  );
 };

1f4bbc73b4aac4edc6fc253d976cc831b31cd8c3
Roman Furman <roman.furman@uptech.team>
2024-11-27 12:00:05 +0200

    add 2.15.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 'use client';
-import { NativelyInfo, useNatively } from 'natively';
+import { NativelyInfo, useNatively, NativelyCamera } from 'natively';
 
 export default function Home() {
 
-  const natively = useNatively();
+
+  const camera = new NativelyCamera()
+const open_camera_callback = function (resp: any) {
+    console.log(resp.base64); // base64 string of media file
+    console.log(resp.content_type); // "image/png"
+    console.log(resp.size); // "1024" <- file size in KB
+};
+const type = "photo" // "photo"/"video"
+const quality = "high" // "high"/"medium"/"low"
+const cameraType = "FRONT" // "FRONT"/"BACK"
+camera.showCamera(type, quality, cameraType, open_camera_callback);
+
+
+  
+  const natively = useNatively(); // === window.natively 
+  // window.natively.openConsole();
+  natively.openConsole();
   const info = new NativelyInfo();
   const browserInfo = info.browserInfo();
   const openConsole = () => {
